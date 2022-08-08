51ea4f2ec3425c36763a3b0903bcf929a30a8fef
SAMMY-VCODE <adekunlesammy10@gmail.com>
2022-08-08 05:53:46 -0700

    intro to java

---
+++ main.js
@@ -0,0 +1,5 @@
+var seconddiv = document.getElementById('orangediv')
+seconddiv.addEventListener('click', function() {
+    alert('orange div is clicked')
+})
+// seconddiv.style.background = "blue"
